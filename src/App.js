5b200e0ccbf06edffa23fe25e70a86c216e9331a
Qodirxon2000uzbek <uzbekqodirxon@gmail.com>
2024-04-29 07:09:22 +0500

    yangi

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './all.css';
 import imagee from './download.png';
+import { BrowserRouter, Route, NavLink, Routes } from "react-router-dom";
+import img1 from './1.png'
+import img2 from './2.png'
+import img3 from './3.png'
+
+
 
 function App() {
   return (
@@ -24,6 +30,32 @@ function App() {
           </div>
         </div>
       </div>
+      <BrowserRouter>
+          <div className="footer">
+             
+              <NavLink to='/home' >
+              <img src={img1} alt="" />
+              </NavLink>
+              <NavLink to='/home' >
+              <img src={img2} alt="" />
+              </NavLink>
+              <NavLink to='/home' >
+              <img src={img3} alt="" />
+              </NavLink>
+
+            <Routes>
+            <Route path='/home' element={<img2 />} />
+            <Route path='/about' element={<img2 />} />
+            <Route path='/basket' element={<img2 />} />
+          </Routes>
+
+
+          </div>
+
+
+         
+          
+          </BrowserRouter>
     </div>
   );
 }
