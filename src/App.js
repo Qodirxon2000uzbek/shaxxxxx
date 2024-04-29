72673227d252a644bc16aedb2d9a93f96ab1a251
Qodirxon2000uzbek <uzbekqodirxon@gmail.com>
2024-04-30 02:43:28 +0500

    first commait

--- src/App.js
+++ src/App.js
@@ -1,62 +1,26 @@
-import React from 'react';
 import './all.css';
-import imagee from './download.png';
-import { BrowserRouter, Route, NavLink, Routes } from "react-router-dom";
-import img1 from './1.png'
-import img2 from './2.png'
-import img3 from './3.png'
-
-
+import image from './download.png'
 
 function App() {
   return (
     <div className="app">
-      <div className="aheader">
-        <img src={imagee} alt="" />
-      </div>
-      <div className="main">
-        <div className="center">
-          <div className="box">
-            <div className="videoas">
-              <iframe
-                src="https://www.youtube.com/embed/a2zg8Ayj-E0?si=u7ythHRF4tbiFJJJ"
-                title="YouTube video player"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              ></iframe>
-              <div className="title">
-                <h3>DINAMO <span>VS</span> NAVBAHOR</h3>
-              </div>
+    <div className="main">
+    <div className="aheader">
+      <img src={image} alt="" />
+    </div>
+      <div className="center">
+        <div className="box">
+          <div className="videoas">
+          <iframe src="https://player.castr.com/live_6ff23a80060a11efadd3e3edb3a24ef1"  frameborder="0" scrolling="no" allow="autoplay" allowfullscreen  webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+            <div className="title">
+              <h3>UZBEKISTON <span>VS</span> INDONEZYA</h3>
             </div>
           </div>
         </div>
       </div>
-      <BrowserRouter>
-          <div className="footer">
-             
-              <NavLink to='/home' >
-              <img src={img1} alt="" />
-              </NavLink>
-              <NavLink to='/home' >
-              <img src={img2} alt="" />
-              </NavLink>
-              <NavLink to='/home' >
-              <img src={img3} alt="" />
-              </NavLink>
-
-            <Routes>
-            <Route path='/home' element={<img2 />} />
-            <Route path='/about' element={<img2 />} />
-            <Route path='/basket' element={<img2 />} />
-          </Routes>
-
-
-          </div>
-
-
-         
-          
-          </BrowserRouter>
     </div>
+    
+  </div>
   );
 }
 
