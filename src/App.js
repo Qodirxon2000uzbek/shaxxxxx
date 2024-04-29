680d0dd6a05f86d8f126ebc5c0dcb1f8abd2bfdb
Qodirxon2000uzbek <uzbekqodirxon@gmail.com>
2024-04-30 03:32:27 +0500

    iltimos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import './all.css';
-import image from './download.png'
+import downloadImage from './download.png';
 
 function App() {
   return (
     <div className="app">
-    <div className="main">
-    <div className="aheader">
-      <img src={image} alt="" />
-    </div>
-      <div className="center">
-        <div className="box">
-          <div className="videoas">
-          <iframe src="https://player.castr.com/live_6ff23a80060a11efadd3e3edb3a24ef1"  frameborder="0" scrolling="no" allow="autoplay" allowfullscreen  webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-            <div className="title">
-              <h3>UZBEKISTON <span>VS</span> INDONEZYA</h3>
+      <div className="main">
+        <div className="aheader">
+          <img src={downloadImage} alt="Download" />
+        </div>
+        <div className="center">
+          <div className="box">
+            <div className="videoas">
+              <iframe src="https://player.castr.com/live_6ff23a80060a11efadd3e3edb3a24ef1" frameborder="0" scrolling="no" allow="autoplay" allowFullScreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+              <div className="title">
+                <h3>UZBEKISTON <span>VS</span> INDONEZYA</h3>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    
-  </div>
   );
 }
 
