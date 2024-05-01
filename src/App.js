a9f45ca718836619d909f7e73f8e1fbf219295a3
Qodirxon2000uzbek <uzbekqodirxon@gmail.com>
2024-05-01 08:29:47 +0500

    bbb

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
+import React, { useState } from 'react';
 import './all.css';
 import downloadImage from './download.png';
-import profile from './a.png'
+import profile from './a.png';
+import bacround from './bg.jpg'
+
+function ModalExample() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return (
+    <>
+      
+      <img src={profile} alt="" onClick={openModal} />
+      {isOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            
+            <p>Ushbu dastur sinov rejimida ishlamoqda!</p>
+            <div className="buttonss">
+            <button onClick={closeModal} >Yopish</button>
+           
+           
+            
+            </div>
+            
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
 
 function App() {
-  alert("Dasturga Xush kelibsiz! ESLATMA USHBU DASTUR DEMO TARZDA ISHLAMOQDA");
-
   return (
     <div className="app">
       <div className="main">
         <div className="aheader">
           <img src={downloadImage} alt="Download" />
-          <div className="profile" >
-          <img src={profile} alt="" />
+          <div className="profile">
+            
+              <ModalExample />
           </div>
-         
         </div>
         <div className="center">
           <div className="box">
             <div className="videoas">
-              <iframe src="https://player.castr.com/live_6ff23a80060a11efadd3e3edb3a24ef1" frameborder="0" scrolling="no" allow="autoplay" allowFullScreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
-              <div className="title">
-                <h3>NAVABHOR <span>VS</span> INDONEZYA</h3>
-              </div>
+              <iframe src="https://player.castr.com/live_ea0e16c006d411efa274cd81b5d28793" frameborder="0" scrolling="no" allow="autoplay" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+            </div>
+            <div className="title">
+              <h3>NAVABHOR <span>VS</span> INDONEZYA</h3>
+            
             </div>
           </div>
         </div>
