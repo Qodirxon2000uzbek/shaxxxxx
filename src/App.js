c43b0407834fa489e4eaa8183b85bc1ed04d5b6d
Qodirxon2000uzbek <uzbekqodirxon@gmail.com>
2024-05-02 08:40:58 +0500

    aa

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ function App() {
         <div className="center">
           <div className="box">
             <div className="videoas">
-            <iframe src="https://player.castr.com/live_ea0e16c006d411efa274cd81b5d28793" frameborder="0" scrolling="no" allow="autoplay" allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>
+            <iframe src="https://player.castr.com/live_ea0e16c006d411efa274cd81b5d28793" frameborder="0" scrolling="no" allow="autoplay" allowfullscreen></iframe>
             </div>
             <div className="title">
               <h3>GROUP 1 <span>VS</span> GROUP 2 </h3>
